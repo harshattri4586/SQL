d08d2f9fb48e9a15864da653e1bb7bbe6dd3bcfd
harshattri4586 <harshzaat@gmail.com>
2026-08-22 13:42:29 +0530

    GROUP BY and aggregate function

---
+++ MyDatabase/SQLQuery5.sql
@@ -0,0 +1,16 @@
+-- Find the total score for each country
+
+SELECT 
+	country,
+	SUM(score) AS total_score,
+	COUNT(id) AS total_customers
+FROM customers
+GROUP BY country
+
+-- Find the total score and total number of customers for each country
+
+SELECT 
+	country,
+	SUM(score) AS total_score
+FROM customers
+GROUP BY country
