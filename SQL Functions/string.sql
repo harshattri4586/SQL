7a0b792f27ac81203a56baace3a05284a01f1aee
harshattri4586 <harshzaat@gmail.com>
2026-08-29 09:00:30 +0530

    Complete string function with its types

--- SQL Functions/string.sql
+++ SQL Functions/string.sql
@@ -38,3 +38,35 @@ WHERE first_name != TRIM(first_name)
 SELECT
 	'123-456-7890' AS phone,
 REPLACE('123-456-7890', '-', '') AS clean_phone
+
+
+-- Calculate the length of each customer's first name.
+
+SELECT
+	first_name,
+	LEN(first_name) AS len_name
+FROM customers
+
+
+-- Retrieve the first two characters of each first name
+
+SELECT
+	first_name,
+	LEFT(first_name, 2) AS first_2_char
+FROM customers
+
+
+-- Retrieve the last two characters of each first name
+
+SELECT
+	first_name,
+	RIGHT(first_name, 2) AS last_2_char 
+FROM customers
+
+
+-- Retrieve the list of customer's first names removing the first character
+
+SELECT
+	first_name,
+	SUBSTRING(first_name, 2, LEN(first_name)) AS sub_name 
+FROM customers
