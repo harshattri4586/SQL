42193b4d0457271af68063c9d43fb3e15861db62
harshattri4586 <harshzaat@gmail.com>
2026-09-10 20:28:26 +0530

    Subquery in sql

--- Advanced SQL/Subquery.sql
+++ Advanced SQL/Subquery.sql
@@ -51,3 +51,63 @@ FROM(
 	FROM Sales.Orders
 	GROUP BY CustomerID
 )t 
+
+
+-- Show the product IDs, product names, prices, and the total number of orders
+
+-- Main Query
+SELECT 
+	ProductID,
+	Product,
+	Price,
+	-- SubQuery
+	(SELECT COUNT(*) TotalOrders FROM Sales.Orders) AS TotalOrders
+FROM Sales.Products;
+
+-- Show all customers details and find the total orders of each customer
+
+-- Main Query
+SELECT
+*
+FROM Sales.Customers c
+LEFT JOIN (
+	SELECT  
+	CustomerID,
+	COUNT(*) TotalOrders
+	FROM Sales.Orders
+	GROUP BY CustomerID
+) o
+ON c.CustomerID = o.CustomerID
+
+
+-- Find the products that have a price higher than the average price of all products
+
+SELECT
+	ProductID,
+	Price
+FROM Sales.Products
+WHERE Price > (SELECT AVG(Price) FROM Sales.Products)
+
+
+--  Show the details of orders made by customers in Germany
+
+SELECT 
+*
+FROM Sales.Orders
+WHERE CustomerID IN (SELECT
+					CustomerID
+					FROM Sales.Customers
+					WHERE Country = 'Germany')
+
+
+-- Find female employees whose salaries are greater 
+-- than the salaries of any male employees
+
+SELECT
+	EmployeeID,
+	FirstName,
+	Gender, 
+	Salary
+FROM Sales.Employees
+WHERE Gender = 'F'
+AND Salary > ANY(SELECT Salary FROM Sales.Employees WHERE Gender = 'M')
